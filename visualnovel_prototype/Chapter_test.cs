3213441f2d38808d9eb9faabe94ec748a350dc7b
tor.bjurbom <tor.bjurbom@5CG6233NTR>
2017-12-04 15:02:53 +0100

    change the textReal

--- visualnovel_prototype/Chapter_test.cs
+++ visualnovel_prototype/Chapter_test.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace visualnovel_prototype
 {
     class Chapter_test
     {
+        
     }
 }
