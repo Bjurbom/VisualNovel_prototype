ca1d03a9b2fb78bbcdce9ef2cd080b94b7eb9321
tor.bjurbom <tor.bjurbom@5CG6233NTR>
2017-12-05 10:13:25 +0100

    Chapter_test update

--- visualnovel_prototype/Chapter_test.cs
+++ visualnovel_prototype/Chapter_test.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace visualnovel_prototype
 {
     class Chapter_test
     {
-        public void story()
-{
-}
+
+        List<string> list;
+
+        public void story(List<string> list)
+        {
+            this.list = list;
+        }
     }
 }
